26297d2a4d9a3dfc9ce2e4a761cc108c2a606bc8
Sushant Gaurav <sushantgaurav57@gmail.com>
2022-01-13 00:18:57 +0530

    first commit

---
+++ Responsive Portfolio/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
+  });
+
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+  });
+
+  $("#theme-toggler").click(function () {
+    $(this).toggleClass("fa-sun");
+    $("body").toggleClass("dark-theme");
+  });
+
+  // smooth scrolling
+
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      500,
+      "linear"
+    );
+  });
+});
